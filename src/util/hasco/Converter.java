028ff3513ba50902e5d84068e74e3f1c0787bfe5
kadirayk <kadiray.k@hotmail.com>
2018-07-31 13:34:43 +0200

    create HASCO conf file for top 10 important paramters PROSECO-36

--- src/util/hasco/Converter.java
+++ src/util/hasco/Converter.java
@@ -4,9 +4,14 @@ import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 import org.commonmark.Extension;
 import org.commonmark.ext.gfm.tables.TableCell;
@@ -31,8 +36,11 @@ public class Converter {
 	private static final int PARAMETER_VALUES = 5;
 	private static final int PARAMETER_EFFECT = 6;
 	private static final int PARAMETER_FILE = 7;
+	private static final int IMPORTANCE_RANK = 8;
 
 	private static Map<String, Component> parsedComponents = new HashMap<>();
+
+	private static Map<Parameter, Integer> parameterRanks = new HashMap<>();
 
 	public static HascoDocument parseConf() {
 		HascoDocument document = new HascoDocument();
@@ -89,6 +97,7 @@ public class Converter {
 			}
 			int cellCounter = 0;
 			Parameter param = new Parameter();
+			param.setComponentName(componentName);
 			while (cell != null) {
 				String val = null;
 				switch (cellCounter) {
@@ -102,7 +111,7 @@ public class Converter {
 					break;
 				case PARAMETER_DEFAULT:
 					val = ((Text) cell.getFirstChild()).getLiteral();
-					if(!val.contains("*")){
+					if (!val.contains("*")) {
 						param.setDefault(val);
 					}
 					break;
@@ -110,6 +119,10 @@ public class Converter {
 					val = ((Text) cell.getFirstChild()).getLiteral();
 					parseValues(param, val);
 					break;
+				case IMPORTANCE_RANK:
+					val = ((Text) cell.getFirstChild()).getLiteral();
+					param.setImportanceRank(Integer.valueOf(val));
+					parameterRanks.put(param, Integer.valueOf(val));
 				default:
 					break;
 				}
@@ -119,15 +132,50 @@ public class Converter {
 			row = (TableRow) row.getNext();
 			parameters.add(param);
 		}
-		
+
+		HascoDocument newDoc = selectMostImportantParameters();
+
 		ObjectMapper mapper = new ObjectMapper();
 		try {
-			mapper.writeValue(new File("resources/hascoDocument.json"), doc);
+			mapper.writeValue(new File("resources/hascoDocument.json"), newDoc);
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 
+	}
+
+	private static HascoDocument selectMostImportantParameters() {
+		List<Component> components = new ArrayList<>();
+		HascoDocument newDoc = new HascoDocument();
+		newDoc.setComponents(components);
+
+		Map<String, Component> addedComponents = new HashMap<>();
+
+		Map<Parameter, Integer> sortedMap = parameterRanks.entrySet().stream().sorted(Collections.reverseOrder(Entry.comparingByValue()))
+				.collect(Collectors.toMap(Entry::getKey, Entry::getValue, (e1, e2) -> e1, LinkedHashMap::new));
+
+		int i = 0;
+		for (Parameter p : sortedMap.keySet()) {
+			if (i == 10) { // get top 10
+				break;
+			}
+			String componentName = p.getComponentName();
+			Component component = null;
+			if (!addedComponents.containsKey(componentName)) {
+				component = new Component();
+				component.setName(componentName);
+				components.add(component);
+				List<Parameter> parameters = new ArrayList<>();
+				component.setParameter(parameters);
+				addedComponents.put(componentName, component);
+			} else {
+				component = addedComponents.get(componentName);
+			}
+			component.getParameter().add(p);
+			i++;
+		}
+		return newDoc;
 	}
 
 	private static void parseValues(Parameter param, String text) {
@@ -141,7 +189,7 @@ public class Converter {
 			} else if (s.contains("max")) {
 				String max = s.split("=")[1];
 				param.setMax(Double.parseDouble(max));
-			} else if (s.contains("*")){
+			} else if (s.contains("*")) {
 				// don't add
 			} else {
 				values.add(s);
--- src/util/hasco/Parameter.java
+++ src/util/hasco/Parameter.java
@@ -12,125 +12,136 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
-@JsonPropertyOrder({
-    "name",
-    "type",
-    "default",
-    "values",
-    "min",
-    "max",
-    "refineSplits",
-    "minInterval"
-})
+@JsonPropertyOrder({ "name", "type", "default", "values", "min", "max", "refineSplits", "minInterval" })
 public class Parameter {
 
-    @JsonProperty("name")
-    private String name;
-    @JsonProperty("type")
-    private String type;
-    @JsonProperty("default")
-    private String _default;
-    @JsonProperty("values")
-    private List<String> values = null;
-    @JsonProperty("min")
-    private Double min;
-    @JsonProperty("max")
-    private Double max;
-    @JsonProperty("refineSplits")
-    private Integer refineSplits;
-    @JsonProperty("minInterval")
-    private Integer minInterval;
-    @JsonIgnore
-    private Map<String, Object> additionalProperties = new HashMap<String, Object>();
+	@JsonProperty("name")
+	private String name;
+	@JsonProperty("type")
+	private String type;
+	@JsonProperty("default")
+	private String _default;
+	@JsonProperty("values")
+	private List<String> values = null;
+	@JsonProperty("min")
+	private Double min;
+	@JsonProperty("max")
+	private Double max;
+	@JsonProperty("refineSplits")
+	private Integer refineSplits;
+	@JsonProperty("minInterval")
+	private Integer minInterval;
+	@JsonIgnore
+	private Integer importanceRank;
+	@JsonIgnore
+	private String componentName;
+	@JsonIgnore
+	private Map<String, Object> additionalProperties = new HashMap<String, Object>();
 
-    @JsonProperty("name")
-    public String getName() {
-        return name;
-    }
+	public Integer getImportanceRank() {
+		return importanceRank;
+	}
 
-    @JsonProperty("name")
-    public void setName(String name) {
-        this.name = name;
-    }
+	public void setImportanceRank(Integer importanceRank) {
+		this.importanceRank = importanceRank;
+	}
 
-    @JsonProperty("type")
-    public String getType() {
-        return type;
-    }
+	public String getComponentName() {
+		return componentName;
+	}
 
-    @JsonProperty("type")
-    public void setType(String type) {
-        this.type = type;
-    }
+	public void setComponentName(String componentName) {
+		this.componentName = componentName;
+	}
 
-    @JsonProperty("default")
-    public String getDefault() {
-        return _default;
-    }
+	@JsonProperty("name")
+	public String getName() {
+		return name;
+	}
 
-    @JsonProperty("default")
-    public void setDefault(String _default) {
-        this._default = _default;
-    }
+	@JsonProperty("name")
+	public void setName(String name) {
+		this.name = name;
+	}
 
-    @JsonProperty("values")
-    public List<String> getValues() {
-        return values;
-    }
+	@JsonProperty("type")
+	public String getType() {
+		return type;
+	}
 
-    @JsonProperty("values")
-    public void setValues(List<String> values) {
-        this.values = values;
-    }
+	@JsonProperty("type")
+	public void setType(String type) {
+		this.type = type;
+	}
 
-    @JsonProperty("min")
-    public Double getMin() {
-        return min;
-    }
+	@JsonProperty("default")
+	public String getDefault() {
+		return _default;
+	}
 
-    @JsonProperty("min")
-    public void setMin(Double min) {
-        this.min = min;
-    }
+	@JsonProperty("default")
+	public void setDefault(String _default) {
+		this._default = _default;
+	}
 
-    @JsonProperty("max")
-    public Double getMax() {
-        return max;
-    }
+	@JsonProperty("values")
+	public List<String> getValues() {
+		return values;
+	}
 
-    @JsonProperty("max")
-    public void setMax(Double max) {
-        this.max = max;
-    }
+	@JsonProperty("values")
+	public void setValues(List<String> values) {
+		this.values = values;
+	}
 
-    @JsonProperty("refineSplits")
-    public Integer getRefineSplits() {
-        return refineSplits;
-    }
+	@JsonProperty("min")
+	public Double getMin() {
+		return min;
+	}
 
-    @JsonProperty("refineSplits")
-    public void setRefineSplits(Integer refineSplits) {
-        this.refineSplits = refineSplits;
-    }
+	@JsonProperty("min")
+	public void setMin(Double min) {
+		this.min = min;
+	}
 
-    @JsonProperty("minInterval")
-    public Integer getMinInterval() {
-        return minInterval;
-    }
+	@JsonProperty("max")
+	public Double getMax() {
+		return max;
+	}
 
-    @JsonProperty("minInterval")
-    public void setMinInterval(Integer minInterval) {
-        this.minInterval = minInterval;
-    }
+	@JsonProperty("max")
+	public void setMax(Double max) {
+		this.max = max;
+	}
 
-    @JsonAnyGetter
-    public Map<String, Object> getAdditionalProperties() {
-        return this.additionalProperties;
-    }
+	@JsonProperty("refineSplits")
+	public Integer getRefineSplits() {
+		return refineSplits;
+	}
 
-    @JsonAnySetter
-    public void setAdditionalProperty(String name, Object value) {
-        this.additionalProperties.put(name, value);
-    }
+	@JsonProperty("refineSplits")
+	public void setRefineSplits(Integer refineSplits) {
+		this.refineSplits = refineSplits;
+	}
+
+	@JsonProperty("minInterval")
+	public Integer getMinInterval() {
+		return minInterval;
+	}
+
+	@JsonProperty("minInterval")
+	public void setMinInterval(Integer minInterval) {
+		this.minInterval = minInterval;
+	}
+
+	@JsonAnyGetter
+	public Map<String, Object> getAdditionalProperties() {
+		return this.additionalProperties;
+	}
+
+	@JsonAnySetter
+	public void setAdditionalProperty(String name, Object value) {
+		this.additionalProperties.put(name, value);
+	}
 
 }
