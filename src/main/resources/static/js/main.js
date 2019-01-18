ab12016238b9d8cc272fcccdbfea26e0638d3d07
kadirayk <kadiray.k@hotmail.com>
2019-01-18 09:24:28 +0100

    changed ui buttons

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,10 +1,29 @@
-function DownloadClient() {
+function StopServer() {
 	$.ajax({
 		type : "GET",
 		contentType : "application/json",
-		url : "/downloadClient",
+		url : "/stopServer/",
 		cache : false,
 		timeout : 60000,
+		success : function(data) {
+			console.log(data);
+		},
+		error : function(e) {
+			console.log("ERROR : ", e);
+		}
+	});
+}
+
+function StartServer() {
+	$.ajax({
+		type : "GET",
+		contentType : "application/json",
+		url : "/startServer/",
+		cache : false,
+		timeout : 60000,
+		success : function(data) {
+			console.log(data);
+		},
 		error : function(e) {
 			console.log("ERROR : ", e);
 		}
