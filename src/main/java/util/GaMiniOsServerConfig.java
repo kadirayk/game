97dec638a1b5d92422a375eb9da610ca2ed3d017
kadirayk <kadiray.k@hotmail.com>
2019-01-25 08:31:15 +0100

    added sample rmi-server.properties

---
+++ src/main/java/util/GaMiniOsServerConfig.java
@@ -0,0 +1,43 @@
+package util;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
+import org.aeonbits.owner.Accessible;
+import org.aeonbits.owner.ConfigFactory;
+import org.aeonbits.owner.Mutable;
+
+public interface GaMiniOsServerConfig extends Mutable, Accessible {
+
+	public static final String RMI_SERVER_IP = "rmi.server_ip";
+	public static final String RMI_SERVER_PORT = "rmi.server_port";
+
+	@Key(RMI_SERVER_IP)
+	@DefaultValue("localhost")
+	public String getRmiServerIp();
+
+	@Key(RMI_SERVER_PORT)
+	@DefaultValue("1099")
+	public Integer getRmiServerPort();
+
+	public static GaMiniOsServerConfig get(String file) {
+		return get(new File(file));
+	}
+
+	public static GaMiniOsServerConfig get(File file) {
+		Properties props = new Properties();
+		try {
+			props.load(new FileInputStream(file));
+		} catch (FileNotFoundException e) {
+			System.err.println("Could not find config file " + file + ". Assuming default configuration");
+		} catch (IOException e) {
+			System.err.println("Encountered problem with config file " + file
+					+ ". Assuming default configuration. Problem:" + e.getMessage());
+		}
+		return ConfigFactory.create(GaMiniOsServerConfig.class, props);
+	}
+
+}
