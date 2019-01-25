04c48509dc6a2db300a79aab0d6a438ffdf31ed6
kadirayk <kadiray.k@hotmail.com>
2019-01-25 11:12:41 +0100

    updated sample prototype

--- sample/game/prototypes/game/deployment.sh
+++ sample/game/prototypes/game/deployment.sh
@@ -1,5 +1,6 @@
 echo Deploying service $1
 
+
 docker run -d -p $3:8080 -t $1 $1 $2 8080 
 
 echo waiting 10 seconds for the service to come up
--- sample/game/prototypes/game/grounding.sh
+++ sample/game/prototypes/game/grounding.sh
@@ -6,5 +6,8 @@ echo Final Gaming Configuration
 # run final gaming configuration inside VM
 java -jar grounding.jar $1 $2 $3
 
+#copying interview_state 
+cp ../../../../processes/$1/interview/interview_state.json strategies/strategy1/interview_state.json
+
 # build docker image for deployment
 docker build -t $1 .
