fcbcebbbc53e9755994f5270bd61826b3aab278d
gokcek <gokcek@mail.uni-paderborn.de>
2019-02-08 15:03:14 +0100

    updated bat files

---
+++ sample/game/prototypes/game/grounding.bat
@@ -0,0 +1,13 @@
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+echo $DIR
+echo Switching to directory $DIR
+cd $DIR
+echo Final Gaming Configuration
+# run final gaming configuration inside VM
+java -jar grounding.jar %1 %2 %3
+
+#copying interview_state 
+copy ../../../../processes/$1/interview/interview_state.json strategies/strategy1/interview_state.json
+
+# build docker image for deployment
+docker build -t $1 .
